07680a964ab149a07fd7589e32cf28662e0321ab
Vitaliy Sobur <vitaliy.sobur@gmail.com>
2023-05-14 18:40:36 +0200

    ~ env vars in the hook

--- modules/addons/addonmodule/hooks.php
+++ modules/addons/addonmodule/hooks.php
@@ -4,7 +4,7 @@ add_hook('AddonActivation', 1, function(array $vars) {
     try {
         $curl = curl_init();
 
-        $base = $vars['is_production'] == true ? 'api.arbon.one' : 'dev-api.arbon.one';
+        $base = ENVIRONMENT == 'production' ? 'api.arbon.one' : 'dev-api.arbon.one';
 
         $url = 'https://' . $base . '/v1/offset?access_token=' . ACCESS_TOKEN . '&amount=' . urlencode('amount') . '&reason=' . urlencode('reason');
 
